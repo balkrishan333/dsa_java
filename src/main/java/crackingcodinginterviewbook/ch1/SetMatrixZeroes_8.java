3d4027d86d2c3bb929e6be78fccf913a5df6fb78
Balkrishan Nagpal <balkrishan.nagpal@gmail.com>
2022-02-18 07:40:37 +0530

    completed set matrix zeros

--- src/main/java/crackingcodinginterviewbook/ch1/SetMatrixZeroes_8.java
+++ src/main/java/crackingcodinginterviewbook/ch1/SetMatrixZeroes_8.java
@@ -7,53 +7,38 @@ import java.util.Set;
 public class SetMatrixZeroes_8 {
 
     public static void main(String[] args) {
-        int[][] matrix = {{1,1,1},{1,0,1},{1,1,1}};
+//        int[][] matrix = {{0,1,2,0},{3,4,5,2},{1,3,1,5}};
+        int[][] matrix = {{0,1}};
 
         setZeroes(matrix);
         System.out.println(Arrays.deepToString(matrix));
     }
 
     private static void setZeroes(int[][] matrix) {
-        Set<Integer> rowNos = new HashSet<>();
-        Set<Integer> colNos = new HashSet<>();
+        boolean[] rows = new boolean[matrix.length];
+        boolean[] cols = new boolean[matrix[0].length];
 
-        for (int i = 0; i < matrix.length - 1; i++) {
-            if (rowNos.contains(i)) {
-                continue;
+        for (int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[i].length; j++) {
+                if(matrix[i][j] == 0) {
+                    rows[i] = true;
+                    cols[j] = true;
+                }
             }
-            for (int j = 0; j < matrix[i].length - 1; j++) {
-                if (colNos.contains(j)) {
-                    continue;
-                }
-                if (matrix[i][j] == 0) {
-                    setCellAndRowToZero(matrix, i,j, rowNos, colNos);
-                }
-
-                if (matrix[i+1][j] == 0) {
-                    setCellAndRowToZero(matrix, i+1,j, rowNos, colNos);
-                }
-
-                if (matrix[i][j+1] == 0) {
-                    setCellAndRowToZero(matrix, i,j+1, rowNos, colNos);
-                }
-
-                if (matrix[i+1][j+1] == 0) {
-                    setCellAndRowToZero(matrix, i+1,j+1, rowNos, colNos);
-                }
-             }
-        }
-    }
-
-    private static void setCellAndRowToZero(int[][] matrix, int i, int j, Set<Integer> rowNos, Set<Integer> colNos) {
-        rowNos.add(i);
-        colNos.add(j);
-
-        for (int k = 0; k < j; k++) {
-            matrix[i][k] = 0;
         }
 
-        for (int k = 0; k < j; k++) {
-            matrix[i][k] = 0;
+        for (int i = 0; i < rows.length; i++) {
+            if (rows[i]) {
+                Arrays.fill(matrix[i], 0);
+            }
+        }
+
+        for (int i = 0; i < cols.length; i++) {
+            if (cols[i]) {
+                for (int row =0; row < matrix.length; row++) {
+                    matrix[row][i]=0;
+                }
+            }
         }
     }
 }
