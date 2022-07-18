4c3dd0ea99538a2ad11402d5429260a2681256c0
Balkrishan Nagpal <balkrishan.nagpal@gmail.com>
2022-07-18 06:37:26 +0530

    LC 283

--- src/main/java/leetcode/_283_MoveZeros.java
+++ src/main/java/leetcode/_283_MoveZeros.java
@@ -1,2 +1,29 @@
-package leetcode;public class _283_MoveZeros {
+package leetcode;
+
+import java.util.Arrays;
+
+public class _283_MoveZeros {
+
+    public static void main(String[] args) {
+        int[] nums = {0,1,0,3,12};
+        _283_MoveZeros obj = new _283_MoveZeros();
+
+        obj.moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public void moveZeroes(int[] nums) {
+        int index = -1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[++index] = nums[i];
+            }
+        }
+
+        index++;
+        while (index < nums.length) {
+            nums[index++] = 0;
+        }
+    }
 }
