a1ecd33068205c346407a76aa3aebd59cf8ea2df
Balkrishan Nagpal <balkrishan.nagpal@gmail.com>
2023-01-31 10:46:57 +0530

    Update _909_SnakesAndLadders.java

--- src/main/java/leetcode/_909_SnakesAndLadders.java
+++ src/main/java/leetcode/_909_SnakesAndLadders.java
@@ -8,13 +8,8 @@ public class _909_SnakesAndLadders {
     public static void main(String[] args) {
         _909_SnakesAndLadders obj = new _909_SnakesAndLadders();
 
-        int[][] board = {{-1,-1,-1,-1,-1,-1},
-                {-1,-1,-1,-1,-1,-1},
-                {-1,-1,-1,-1,-1,-1},
-                {-1,35,-1,-1,13,-1},
-                {-1,-1,-1,-1,-1,-1},
-                {-1,15,-1,-1,-1,-1}
-        };
+        int[][] board = {{-1, -1, -1, -1, -1, -1}, {-1, -1, -1, -1, -1, -1}, {-1, -1, -1, -1, -1, -1},
+                {-1, 35, -1, -1, 13, -1}, {-1, -1, -1, -1, -1, -1}, {-1, 15, -1, -1, -1, -1}};
 
         System.out.println(obj.snakesAndLadders(board));
     }
@@ -25,9 +20,7 @@ public class _909_SnakesAndLadders {
         int n = board.length;
         boolean[] visited = new boolean[n * n + 1];
 
-        //explore each cell
         for (int move = 0; !queue.isEmpty(); move++) {
-            //for each cell explore all possibilities
             for (int size = queue.size(); size > 0; size--) {
                 int num = queue.poll();
                 if (visited[num]) {
@@ -36,14 +29,11 @@ public class _909_SnakesAndLadders {
                 visited[num] = true;
 
                 if (num == n * n) {
-                    //return if we reach to last cell(top right) on board
                     return move;
                 }
-
-                //from each cell there are 6 possibilities, as there are 6 faces on a dice
                 for (int i = 1; i <= 6 && num + i <= n * n; i++) {
-                    int next = num + i; //by default next is current + dice value
-                    int val = getNext(board, next); // check if the next has ladder or snake
+                    int next = num + i;
+                    int val = getNext(board, next);
 
                     if (val > 0) {
                         next = val;
@@ -60,8 +50,11 @@ public class _909_SnakesAndLadders {
     int getNext(int[][] board, int num) {
 
         int n = board.length;
-        int r = (num - 1) / n; //find row
-        int row = n - 1 - r; //n-1 because matrix is in reverse order, rows with higher number at top
+        int r = (num - 1) / n;
+
+        //n-1 because matrix is in reverse order. r would have worked if the matrix was from top to bottom
+        // but input matrix is bottom to up
+        int row = n - 1 - r;
 
         // r*n == no of elements in earlier rows
         int col = r % 2 == 0 ? num - 1 - r * n : n + r * n - num;
