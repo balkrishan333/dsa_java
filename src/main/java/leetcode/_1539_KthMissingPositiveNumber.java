bb6f592d5c645cd3fe0062759b38c9da307ed576
Nagpal, Balkrishan <balkrishan.nagpal@optum.com>
2023-03-07 16:51:18 +0530

    Updated comments

--- src/main/java/leetcode/_1539_KthMissingPositiveNumber.java
+++ src/main/java/leetcode/_1539_KthMissingPositiveNumber.java
@@ -10,16 +10,35 @@ public class _1539_KthMissingPositiveNumber {
         System.out.println(obj.findKthPositive(arr, k));
     }
 
-    public int findKthPositive(int[] A, int k) {
-        int l = 0, r = A.length, m;
-        while (l < r) {
-            m = (l + r) / 2;
-            if (A[m] - 1 - m < k)
-                l = m + 1;
+    /*
+
+
+    Need to understand it. Have very high level understanding
+
+    arr[mid]: if there are no missing positives, then the number of positives till mid is arr[mid]
+    mid: mid + 1 is the actual number of positives till index mid (+1 is because the positives start from 1)
+    arr[mid] - (mid + 1): number of missing positives at index mid
+    if the number of missing positives at index mid is less than k (arr[mid] - (mid + 1) < k), it means we didn't find
+    the position for kth positive and so left = mid + 1
+    The same for arr[mid] - (mid + 1) > k
+
+    The BS helps to find that kth positive is between arr[left - 1] and arr[left]:
+
+    The number of missing positives at index left - 1: arr[left - 1] - (left - 1 + 1) = arr[left - 1] - left
+    The number of missing positives from index left - 1 to k: k - (arr[left - 1] - left)
+    The actual kth value starting from arr[left - 1]: arr[left - 1] + {k - (arr[left - 1] - l)} = k + left
+     */
+    public int findKthPositive(int[] arr, int k) {
+
+        int left = 0, right = arr.length;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] - 1 - mid < k)
+                left = mid + 1;
             else
-                r = m;
+                right = mid;
         }
-        return l + k;
+        return left + k;
     }
 
     public int findKthPositive_linear(int[] arr, int k) {
