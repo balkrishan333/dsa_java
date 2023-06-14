cc48005f5f35315a881689f2ae0e8f42c3bba0bd
Balkrishan Nagpal <balkrishan.nagpal@gmail.com>
2023-06-14 06:00:47 +0530

    LC - 530

---
+++ src/main/java/leetcode/_530_MinimumAbsoluteDifferenceInBST.java
@@ -0,0 +1,44 @@
+package leetcode;
+
+import leetcode.common.BinaryTreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _530_MinimumAbsoluteDifferenceInBST {
+
+    public static void main(String[] args) {
+        _530_MinimumAbsoluteDifferenceInBST obj = new _530_MinimumAbsoluteDifferenceInBST();
+
+        BinaryTreeNode root = new BinaryTreeNode().createTree(new Integer[]{1,0,48,null,null,12,49});
+        System.out.println(obj.getMinimumDifference(root));
+    }
+
+    //since given tree is a BST, we cna use list otherwise we need to use Priority Queue
+    private List<Integer> list = new ArrayList<>();
+
+    /*
+        Approach: Inorder traversal with list
+     */
+    public int getMinimumDifference(BinaryTreeNode root) {
+        inorderTraversal(root);
+
+
+        int answer = Integer.MAX_VALUE;
+
+        for (int i = 1; i < list.size(); i++) {
+            answer = Math.min(answer, list.get(i) - list.get(i-1));
+        }
+        return answer;
+    }
+
+    private void inorderTraversal(BinaryTreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        inorderTraversal(node.left);
+        list.add(node.val);
+        inorderTraversal(node.right);
+    }
+}
