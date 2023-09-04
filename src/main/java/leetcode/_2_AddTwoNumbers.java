9f72f622701d34bd2b82b87f45fbd03be18398e3
Balkrishan Nagpal <balkrishan.nagpal@gmail.com>
2023-09-04 14:10:41 +0530

    LC - 2 - Refactoring

--- src/main/java/leetcode/_2_AddTwoNumbers.java
+++ src/main/java/leetcode/_2_AddTwoNumbers.java
@@ -17,36 +17,23 @@ public class _2_AddTwoNumbers {
 
         ListNode result = null, prev=null;
 
-        if(l1 == null && l2 == null) {
-            return null;
-        }
-
-        if(l1 == null) {
-            return l2;
-        }
-
-        if(l2 == null) {
-            return l1;
-        }
-
         int carry = 0;
+        while(l1 != null || l2 != null || carry == 1) {
+            int val1 = 0, val2 = 0;
 
-        while(l1 != null || l2 != null) {
-            int val1 = 0, val2=0;
-
+            int sum = carry;
             if(l1 != null) {
-                val1 = l1.val;
+                sum  += l1.val;
                 l1 = l1.next;
             }
 
             if(l2 != null) {
-                val2 = l2.val;
+                sum += l2.val;
                 l2 = l2.next;
             }
 
-            int add = val1+val2+carry;
-            carry = add / 10;
-            int val = add % 10;
+            carry = sum / 10;
+            int val = sum % 10;
 
             if(result == null) {
                 result = new ListNode();
@@ -59,14 +46,6 @@ public class _2_AddTwoNumbers {
                 prev = node;
             }
         }
-
-        if(carry != 0) {
-            ListNode node = new ListNode();
-            node.val = carry;
-            prev.next = node;
-        }
-
         return result;
-
     }
 }
