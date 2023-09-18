f2e436af6982707dda37689f9b7ae5e459dd6ade
Balkrishan Nagpal <balkrishan.nagpal@gmail.com>
2023-09-18 12:02:38 +0530

    LC - 1584 - Complete

--- src/main/java/leetcode/_1584_MinCostToConnectAllPoints.java
+++ src/main/java/leetcode/_1584_MinCostToConnectAllPoints.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Arrays;
+
 public class _1584_MinCostToConnectAllPoints {
 
     public static void main(String[] args) {
@@ -10,28 +12,30 @@ public class _1584_MinCostToConnectAllPoints {
     }
 
     public int minCostConnectPoints(int[][] points) {
-
         int edges = 0;
         int result = 0;
         boolean[] visited = new boolean[points.length];
-        int prev = 0;
         int curr = 0;
         visited[curr] = true;
+        int[] distArr = new int[points.length];
+        Arrays.fill(distArr, Integer.MAX_VALUE);
 
         while (edges < points.length-1) {
-            int min = Integer.MAX_VALUE;
+            int next = -1;
+            int minEdge = Integer.MAX_VALUE;
             for (int i = 0; i < points.length; i++) {
                 if (!visited[i]) {
-                    int cost = Math.abs(points[prev][0] - points[i][0]) + Math.abs(points[prev][1] - points[i][1]);
-                    if (cost < min) {
-                        min = cost;
-                        curr = i;
+                    int cost = Math.abs(points[curr][0] - points[i][0]) + Math.abs(points[curr][1] - points[i][1]);
+                    distArr[i] = Math.min(cost, distArr[i]);
+                    if (distArr[i] < minEdge) {
+                        minEdge = distArr[i];
+                        next = i;
                     }
                 }
             }
             edges++;
-            prev = curr;
-            result += min;
+            curr = next;
+            result += minEdge;
             visited[curr] = true;
         }
         return result;
