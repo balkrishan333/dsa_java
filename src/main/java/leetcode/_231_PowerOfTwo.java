1baa20225f57a5433df8825377c84bbf4d308be2
Balkrishan Nagpal <balkrishan.nagpal@gmail.com>
2023-12-28 08:01:56 +0530

    LC - 231 - Correction

--- src/main/java/leetcode/_231_PowerOfTwo.java
+++ src/main/java/leetcode/_231_PowerOfTwo.java
@@ -10,9 +10,6 @@ public class _231_PowerOfTwo {
     }
 
     public boolean isPowerOfTwo(int n) {
-       if (n == 0) {
-           return false;
-       }
-       return Integer.bitCount(n) == 1;
+       return n > 0 && Integer.bitCount(n) == 1;
     }
 }
