9f5d8774b34379a204b15a88f2d7813e98e2ca74
Balkrishan Nagpal <balkrishan.nagpal@gmail.com>
2024-05-10 18:02:16 +0530

    LC-786

--- src/main/java/leetcode/_786_KTH_SmallestPrimeFraction.java
+++ src/main/java/leetcode/_786_KTH_SmallestPrimeFraction.java
@@ -9,13 +9,13 @@ public class _786_KTH_SmallestPrimeFraction {
     public static void main(String[] args) {
         _786_KTH_SmallestPrimeFraction obj = new _786_KTH_SmallestPrimeFraction();
 
-        int[] arr = {1,2,3,5};
-        int k = 3;
+        int[] arr = {1,7};
+        int k = 1;
         System.out.println(Arrays.toString(obj.kthSmallestPrimeFraction(arr, k)));
     }
 
     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
-        PriorityQueue<double[]> minHeap = new PriorityQueue<>(Comparator.comparingDouble(a-> a[0]));
+        PriorityQueue<double[]> minHeap = new PriorityQueue<>((a, b) -> Double.compare(b[0], a[0]));
 
         int j = arr.length-1;
         for (int i = 0; i < arr.length; i++) {
@@ -24,16 +24,17 @@ public class _786_KTH_SmallestPrimeFraction {
 
         while (k > 1 && !minHeap.isEmpty()) {
             double[] top = minHeap.poll();
-            int numerator = (int)top[1];
-            int denominator = (int)top[2];
+            int numeratorIndex = (int)top[1];
+            int denominatorIndex = (int)top[2];
 
-            denominator--; // next denominator to consider
+            denominatorIndex--; // next denominatorIndex to consider
 
-            if (denominator > numerator) {
-                minHeap.offer(new double[]{-1.0 * arr[numerator]/arr[denominator], numerator, denominator});
+            if (denominatorIndex > numeratorIndex) {
+                minHeap.offer(new double[]{-1.0 * arr[numeratorIndex]/arr[denominatorIndex], numeratorIndex, denominatorIndex});
             }
+            k--;
         }
         double[] answer = minHeap.poll();
-        return new int[]{(int)answer[1], (int)answer[2]};
+        return new int[]{arr[(int)answer[1]], arr[(int)answer[2]]};
     }
 }
