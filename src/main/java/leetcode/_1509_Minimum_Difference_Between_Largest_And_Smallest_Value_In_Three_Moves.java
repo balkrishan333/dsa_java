7d0703765941e3c791bc118590dd0f821116104b
Balkrishan Nagpal <balkrishan.nagpal@gmail.com>
2024-07-03 06:00:11 +0530

    LC - 1509 - documentation

--- src/main/java/leetcode/_1509_Minimum_Difference_Between_Largest_And_Smallest_Value_In_Three_Moves.java
+++ src/main/java/leetcode/_1509_Minimum_Difference_Between_Largest_And_Smallest_Value_In_Three_Moves.java
@@ -24,9 +24,24 @@ public class _1509_Minimum_Difference_Between_Largest_And_Smallest_Value_In_Thre
 
         Arrays.sort(nums);
 
+        /*
+            There are four possible optimal scenarios
+
+            1. Removing the three largest elements.
+            2. Removing the two largest and one smallest elements.
+            3. Removing one largest and two smallest elements.
+            4. Removing the three smallest elements.
+
+            For an array of size 10, it would mean
+            1. difference between (9, 3)
+            2. difference between (8, 2)
+            3. difference between (7, 1)
+            4. difference between (6, 0)
+         */
+
         int result = Integer.MAX_VALUE;
         for (int i = 0; i < 4; i++) {
-            result = Math.min(result, nums[nums.length-1-i] - nums[3-i]);
+            result = Math.min(result, nums[nums.length - 1 - i] - nums[3 - i]);
         }
         return result;
     }
