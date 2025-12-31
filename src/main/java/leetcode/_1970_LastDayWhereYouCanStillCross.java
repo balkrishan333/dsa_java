0f717b714484382cdcd7582a418dbea79be668ad
Balkrishan Nagpal <balkrishan.nagpal@gmail.com>
2025-12-31 08:11:56 +0530

    LC-1970 - Documentation

--- src/main/java/leetcode/_1970_LastDayWhereYouCanStillCross.java
+++ src/main/java/leetcode/_1970_LastDayWhereYouCanStillCross.java
@@ -21,7 +21,7 @@ public class _1970_LastDayWhereYouCanStillCross {
         cells from end and convert water cells back to land and check when top and bottom row connect. When they
         connect that is the answer. On that day we can cross the grid after that there will be disconnection.
 
-        When we traverse a cell, we see adjacent cells and any of them is land we union them to be part of same group.
+        When we traverse a cell, we see adjacent cells and if any of them is land we union them to be part of same group.
 
         We keep 2 extra cells, one for top row and one for bottom row and check connectivity for these 2. This helps
         to make sure we don't have to check connectivity of each cell from top row to each cell in bottom row.
@@ -29,15 +29,22 @@ public class _1970_LastDayWhereYouCanStillCross {
     public int latestDayToCross(int row, int col, int[][] cells) {
         int[][] grid = new int[row][col];
         for (int i = 0; i < row; i++) {
-            Arrays.fill(grid[i], 1);
+            Arrays.fill(grid[i], 1);  // 1 = water, 0 = land, Initially everything is water
         }
 
         UnionFind unionFind = new UnionFind(row*col+2); // 2 extra cells, one for top row and bottom row
         int[][] directions = new int[][]{{0,1}, {0,-1}, {1,0}, {-1,0}};
 
         for (int i = cells.length-1; i >= 0; i--) {
-            int currRow = cells[i][0]-1;
-            int currCol = cells[i][1]-1;
+            int currRow = cells[i][0]-1; //-1 because grid is 1 based
+            int currCol = cells[i][1]-1; //-1 because grid is 1 based
+
+            /*
+                Following step is required to convert 2D array to 1D array because union find works with 1D array
+                Each row has col cells and Row r starts after r * col elements. this means a unique id for each cell in the grid
+
+                +1 because top row is placeholder (we added 2 extra cells one at top and one at bottom)
+             */
             int currUnionFindIndex = currRow * col + currCol +1;
 
             grid[currRow][currCol] = 0;
